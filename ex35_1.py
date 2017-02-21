c8831520685beaf3e45fd285330ec12dceb72916
Xiaoxuan LIU <cumtlxx@hotmail.com>
2017-02-21 20:46:14 +0800

    add ex35_1

---
+++ ex35_1.py
@@ -0,0 +1,11 @@
+num = 2
+if num > 3:
+	print "This is good."
+elif num > 5:
+	print "This is very good."
+elif num < 13:
+	print "This is a good boy."
+else:
+	print "I am a good girl."
+	
+print False and False
